d62cadb3fd78eabac52f7742711df1274d419e42
zalakajr <zalakajunior12345@gmail.com>
2025-03-10 02:21:04 +0100

    initial commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,17 @@
+
+
+const ListGroup = () => {
+  return (
+    <div>
+          <ul className="list-group">
+        <li className="list-group-item">An Item</li>
+        <li className="list-group-item">An Item</li>
+        <li className="list-group-item">An Item</li>
+        <li className="list-group-item">An Item</li>
+        <li className="list-group-item">An Item</li>
+    </ul>
+    </div>
+  )
+}
+
+export default ListGroup
